82fed37e292574ba67eab8f462fc47d86f716cdb
KomalSita <komalsita42@gmail.com>
2026-02-27 23:59:12 +0530

    String Codes

--- src/main/java/org/BasicCoding/StringDemo.java
+++ src/main/java/org/BasicCoding/StringDemo.java
@@ -38,7 +38,7 @@ public class StringDemo {
             for(int i=word.length()-1; i>=0; i--) {
                revWord=revWord+word.charAt(i);
             }
-            output +=  revWord+ " ";
+            output = output + revWord+ " ";
             }
         System.out.println("revered string:- " + output);
 
